624259b26710f126aa087018206869fde9069413
dotwoo <dotwoo@gmail.com>
2021-03-23 17:11:12 +0800

    feat(main): read req body

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /*
  * @Author: dotwoo
  * @Date: 2021-03-23 10:28:53
- * @LastEditTime: 2021-03-23 16:54:01
+ * @LastEditTime: 2021-03-23 17:07:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rust_unit_app/src/main.rs
@@ -10,16 +10,21 @@
 #![feature(core_intrinsics)]
 
 use gflags;
+use rust_unit_app::nxt_unit_buf_send;
+use rust_unit_app::nxt_unit_buf_t;
 use rust_unit_app::nxt_unit_done;
 use rust_unit_app::nxt_unit_init;
 use rust_unit_app::nxt_unit_init_t;
 use rust_unit_app::nxt_unit_request_done;
 use rust_unit_app::nxt_unit_request_info_t;
+use rust_unit_app::nxt_unit_request_read;
 use rust_unit_app::nxt_unit_response_add_content;
 use rust_unit_app::nxt_unit_response_add_field;
+use rust_unit_app::nxt_unit_response_buf_alloc;
 use rust_unit_app::nxt_unit_response_init;
 use rust_unit_app::nxt_unit_response_send;
 use rust_unit_app::nxt_unit_run;
+use rust_unit_app::size_t;
 use rust_unit_app::uint16_t;
 use rust_unit_app::uint32_t;
 use rust_unit_app::uint8_t;
@@ -99,5 +104,30 @@ unsafe extern "C" fn greeting_app_request_handler(req: *mut nxt_unit_request_inf
 
     rc = nxt_unit_response_send(req);
 
+    if std::intrinsics::unlikely(rc != rust_unit_app::NXT_UNIT_OK as i32) {
+        nxt_unit_request_done(req, rc);
+        return;
+    }
+
+    let buf = nxt_unit_response_buf_alloc(
+        req,
+        (*(*req).request_buf)
+            .end
+            .offset_from((*(*req).request_buf).start) as u32,
+    );
+
+    if std::intrinsics::unlikely(buf == 0 as *mut libc::c_void as *mut nxt_unit_buf_t) {
+        nxt_unit_request_done(req, rc);
+        return;
+    }
+
+    let res = nxt_unit_request_read(
+        req,
+        (*buf).free as *mut libc::c_void,
+        (*buf).end.offset_from((*buf).free) as libc::c_long as size_t,
+    );
+    (*buf).free = (*buf).free.offset(res as isize);
+
+    rc = nxt_unit_buf_send(buf);
     nxt_unit_request_done(req, rc);
 }
