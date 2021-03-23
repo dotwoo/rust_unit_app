d7e787ef81727dd36b862b6b2a80479f06262009
dotwoo <dotwoo@gmail.com>
2021-03-23 16:04:10 +0800

    feat(main): response add field

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /*
  * @Author: dotwoo
  * @Date: 2021-03-23 10:28:53
- * @LastEditTime: 2021-03-23 15:45:22
+ * @LastEditTime: 2021-03-23 16:01:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rust_unit_app/src/main.rs
@@ -50,7 +50,9 @@ gflags::define! {
     -h, --help = false
 }
 
-static RETURNCONTENT: &str = "Rust: Hello world!";
+static RETURNCONTENT: &str = "Rust: Hello world!\n";
+static CONTENTTYPE: &str = "Content-Type";
+static TEXTPLAIN: &str = "text/plain";
 
 fn main() {
     gflags::parse();
@@ -65,15 +67,14 @@ unsafe fn uxt_main(threads: i32) {
     let mut ctx: *mut nxt_unit_ctx_t = 0 as *mut nxt_unit_ctx_t;
     let mut init: nxt_unit_init_t = unsafe { mem::zeroed() };
     init.callbacks.request_handler = Some(greeting_app_request_handler);
-    unsafe {
-        ctx = nxt_unit_init(&mut init);
-        if ctx.is_null() {
-            std::process::exit(1);
-        }
-        nxt_unit_run(ctx);
 
-        nxt_unit_done(ctx);
+    ctx = nxt_unit_init(&mut init);
+    if ctx.is_null() {
+        std::process::exit(1);
     }
+    nxt_unit_run(ctx);
+
+    nxt_unit_done(ctx);
 }
 
 unsafe extern "C" fn greeting_app_request_handler(mut req: *mut nxt_unit_request_info_t) {
@@ -82,7 +83,14 @@ unsafe extern "C" fn greeting_app_request_handler(mut req: *mut nxt_unit_request
         req,
         200 as uint16_t,
         1 as uint32_t,
-        RETURNCONTENT.len() as uint32_t,
+        (CONTENTTYPE.len() + TEXTPLAIN.len() + RETURNCONTENT.len()) as uint32_t,
+    );
+    rc = nxt_unit_response_add_field(
+        req,
+        CONTENTTYPE.as_ptr() as *const libc::c_char,
+        CONTENTTYPE.len() as uint8_t,
+        TEXTPLAIN.as_ptr() as *const libc::c_char,
+        TEXTPLAIN.len() as uint32_t,
     );
     rc = nxt_unit_response_add_content(
         req,
